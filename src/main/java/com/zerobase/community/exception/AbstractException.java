455bad9f5ec645eb8ec302414d059e39c7fd9cbc
JinSSSol <48242158+JinSSSol@users.noreply.github.com>
2023-03-29 21:15:58 +0900

    Delete AbstractException.java

--- src/main/java/com/zerobase/community/exception/AbstractException.java
+++
@@ -1,7 +0,0 @@
-package com.zerobase.community.exception;
-
-public abstract class AbstractException extends RuntimeException {
-
-	abstract public int getStatusCode();
-	abstract public String getMessage();
-}
